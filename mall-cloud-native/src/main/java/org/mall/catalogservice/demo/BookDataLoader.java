01544c88ea6afab86cfd4b17f49c665b0e596bc6
yingtingxu <yingtingxu@tencent.com>
2024-06-05 19:19:34 +0800

    feat: add test data load
    this is for demo usage of Profile & ConditionalOnProperty features

---
+++ mall-cloud-native/src/main/java/org/mall/catalogservice/demo/BookDataLoader.java
@@ -0,0 +1,28 @@
+package org.mall.catalogservice.demo;
+
+import org.mall.catalogservice.domain.Book;
+import org.mall.catalogservice.domain.BookRepository;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Profile("testdata")
+@ConditionalOnProperty(name="mall.testdata.enabled", havingValue = "true")
+public class BookDataLoader {
+    private final BookRepository bookRepository;
+
+    public BookDataLoader(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void loadBookTestData() {
+        var book1 = new Book("1234567891", "Northern Lights", "Lyra Silverstar", 9.90);
+        var book2 = new Book("1234567892", "Polar Journey", "Iorek Polarson", 12.90);
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+    }
+}
