7e7b10a73dab3aa39ff14ce78621637de3fe2278
yingtingxu <yingtingxu@tencent.com>
2024-06-05 17:36:21 +0800

    feat: use spring boot configuration processor to generate metadata about custom properties

---
+++ mall-cloud-native/src/main/java/org/mall/catalogservice/config/MallProperties.java
@@ -0,0 +1,19 @@
+package org.mall.catalogservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "mall")
+public class MallProperties {
+    /**
+     * A message to welcome users.
+     */
+    private String greeting;
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+}
--- mall-cloud-native/src/main/java/org/mall/catalogservice/web/HomeController.java
+++ mall-cloud-native/src/main/java/org/mall/catalogservice/web/HomeController.java
@@ -1,12 +1,19 @@
 package org.mall.catalogservice.web;
 
+import org.mall.catalogservice.config.MallProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    private final MallProperties mallProperties;
+
+    public HomeController(MallProperties mallProperties) {
+        this.mallProperties = mallProperties;
+    }
+
     @GetMapping("/")
     public String getGreeting() {
-        return "Welcome to the book catalog service!";
+        return mallProperties.getGreeting();
     }
 }
