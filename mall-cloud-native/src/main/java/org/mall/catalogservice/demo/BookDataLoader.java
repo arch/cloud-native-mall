84f85e135e00040b81c2bd4702405068552d72c8
yingtingxu <yingtingxu@tencent.com>
2024-06-05 20:15:17 +0800

    style: remove unused import statements

--- mall-cloud-native/src/main/java/org/mall/catalogservice/demo/BookDataLoader.java
+++ mall-cloud-native/src/main/java/org/mall/catalogservice/demo/BookDataLoader.java
@@ -4,7 +4,6 @@ import org.mall.catalogservice.domain.Book;
 import org.mall.catalogservice.domain.BookRepository;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
