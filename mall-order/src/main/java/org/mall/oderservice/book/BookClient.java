af722cfa35978b243e85af63540bc40b846509c3
yingtingxu <yingtingxu@tencent.com>
2024-06-12 19:16:08 +0800

    Add retry & error resume for container services interaction

--- mall-order/src/main/java/org/mall/oderservice/book/BookClient.java
+++ mall-order/src/main/java/org/mall/oderservice/book/BookClient.java
@@ -2,7 +2,11 @@ package org.mall.oderservice.book;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Component
 public class BookClient {
@@ -17,6 +21,12 @@ public class BookClient {
                 .get()
                 .uri("/books/{isbn}", isbn)
                 .retrieve()
-                .bodyToMono(Book.class);
+                .bodyToMono(Book.class)
+                .timeout(Duration.ofSeconds(3), Mono.empty())
+                .onErrorResume(
+                        WebClientResponseException.NotFound.class, exception -> Mono.empty())
+                .retryWhen(
+                        Retry.backoff(3, Duration.ofMillis(100)))
+                .onErrorResume(Exception.class, ex -> Mono.empty());
     }
 }
