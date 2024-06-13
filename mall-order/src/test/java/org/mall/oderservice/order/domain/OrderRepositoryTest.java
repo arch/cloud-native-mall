a055b765d4758c6ff3a01ce7d584050027d23be8
yingtingxu <yingtingxu@tencent.com>
2024-06-13 11:04:14 +0800

    Add order unit tests & integration tests

---
+++ mall-order/src/test/java/org/mall/oderservice/order/domain/OrderRepositoryTest.java
@@ -0,0 +1,56 @@
+package org.mall.oderservice.order.domain;
+
+import org.junit.jupiter.api.Test;
+import org.mall.oderservice.config.DatabaseConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import reactor.test.StepVerifier;
+
+@DataR2dbcTest
+@Import(DatabaseConfig.class)
+@Testcontainers
+class OrderRepositoryTest {
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.r2dbc.url", OrderRepositoryTest::r2dbcUrl);
+        registry.add("spring.r2dbc.username", postgres::getUsername);
+        registry.add("spring.r2dbc.password", postgres::getPassword);
+        registry.add("spring.flyway.url", postgres::getJdbcUrl);
+    }
+
+    private static String r2dbcUrl() {
+        return String.format("r2dbc:postgresql://%s:%s/%s",
+                postgres.getHost(),
+                postgres.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT),
+                postgres.getDatabaseName());
+    }
+
+    @Test
+    void createRejectedOrder() {
+        var rejectedOrder = OrderService.buildRejectedOrder("1234567890", 3);
+        StepVerifier.create(orderRepository.save(rejectedOrder))
+                .expectNextMatches(
+                        order -> order.status().equals(OrderStatus.REJECTED))
+                .verifyComplete();
+    }
+
+
+    @Test
+    void findOrderByIdWhenNotExisting() {
+        StepVerifier.create(orderRepository.findById(394L))
+                .expectNextCount(0)
+                .verifyComplete();
+    }
+}
---
+++ mall-order/src/test/java/org/mall/oderservice/order/web/OrderControllerTest.java
@@ -0,0 +1,44 @@
+package org.mall.oderservice.order.web;
+
+import org.junit.jupiter.api.Test;
+import org.mall.oderservice.order.domain.Order;
+import org.mall.oderservice.order.domain.OrderService;
+import org.mall.oderservice.order.domain.OrderStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@WebFluxTest(OrderController.class)
+class OrderControllerTest {
+    @Autowired
+    private WebTestClient webClient;
+
+    @MockBean
+    private OrderService orderService;
+
+    @Test
+    void whenBookNotAvailableThenRejectedOrder() {
+        var orderRequest = new OrderRequest("1234567890",3);
+        var expectedOrder = OrderService.buildRejectedOrder(
+                orderRequest.isbn(), orderRequest.quantity());
+
+        given(orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity()))
+                .willReturn(Mono.just(expectedOrder));
+
+        webClient.post()
+                .uri("/orders")
+                .bodyValue(orderRequest)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(Order.class).value(actualOrder -> {
+                   assertThat(actualOrder).isNotNull();
+                   assertThat(actualOrder.status()).isEqualTo(OrderStatus.REJECTED);
+                });
+    }
+
+}
