90238e45c03bdfcb4d00311ffbee7ed0fc9b3b70
yingtingxu <yingtingxu@tencent.com>
2024-06-16 19:58:18 +0800

    Exclude UserDetail auto configuration

--- mall-edge/src/main/java/org/mall/edgeservice/Application.java
+++ mall-edge/src/main/java/org/mall/edgeservice/Application.java
@@ -2,8 +2,9 @@ package org.mall.edgeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
